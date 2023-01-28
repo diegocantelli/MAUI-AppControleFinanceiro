429cc553ea90bed94efe1b7df529294c33fed68b
diegocantelli <diegocantelli19@gmail.com>
2023-01-28 15:41:42 -0300

    Criando o repositorio de transacao

---
+++ AppControleFinanceiro/Repositories/ITransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Transactions;
+
+namespace ControleFinanceiro.Repositories
+{
+	public interface ITransactionRepository
+	{
+        void Add(Transaction transaction);
+        void Delete(Transaction transaction);
+        List<Transaction> GetAll();
+        void Update(Transaction transaction);
+    }
+}
+
---
+++ AppControleFinanceiro/Repositories/TransactionRepositories.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Transactions;
+
+namespace ControleFinanceiro.Repositories
+{
+	public class TransactionRepositories : ITransactionRepository
+	{
+		public TransactionRepositories()
+		{
+		}
+
+        public void Add(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
