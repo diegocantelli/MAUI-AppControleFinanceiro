6881ed22f37ffd0317275590b761fb607edc33be
diegocantelli <diegocantelli19@gmail.com>
2023-01-28 15:32:25 -0300

    Initial commit

---
+++ AppControleFinanceiro/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ControleFinanceiro.Models
+{
+	public class Transaction
+	{
+        public int Id { get; set; }
+        public TransactionType Type { get; set; }
+        public String Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public double Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expense
+    }
+}
+
