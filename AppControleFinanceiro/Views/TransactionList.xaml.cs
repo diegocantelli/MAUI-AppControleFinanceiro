580d278b8ef5a5f74021296d29b1166da6e9de55
diegocantelli <diegocantelli19@gmail.com>
2023-01-30 20:53:02 -0300

    Listando as transacoes e atraves de collectionView e DataTemplate

--- AppControleFinanceiro/Views/TransactionList.xaml.cs
+++ AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -4,14 +4,19 @@ public partial class TransactionList : ContentPage
 {
     private readonly TransactionAdd _transactionAdd;
     private readonly TransactionEdit _transactionEdit;
+    private readonly Repositories.ITransactionRepository _transactionRepository;
 
     public TransactionList(
 		TransactionAdd transactionAdd,
-        TransactionEdit transactionEdit)
+        TransactionEdit transactionEdit,
+        Repositories.ITransactionRepository transactionRepository)
 	{
 		InitializeComponent();
 		_transactionAdd = transactionAdd;
 		_transactionEdit = transactionEdit;
+        _transactionRepository = transactionRepository;
+
+        CollectionViewTransactions.ItemsSource = _transactionRepository.GetAll();
     }
 
 	private void OnButtonClicked_GoTo_TransactionAdd(object sender, EventArgs e)
