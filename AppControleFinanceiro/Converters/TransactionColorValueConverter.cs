3fb8f59e7c4f6db1b11fa1560d175b505d74514e
diegocantelli <diegocantelli19@gmail.com>
2023-02-01 14:12:35 -0300

    Utilizando conversores para formatar a saida de alguns dados

---
+++ AppControleFinanceiro/Converters/TransactionColorValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleFinanceiro.Converters
+{
+    public class TransactionColorValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var transaction = (Models.Transaction)value;
+
+            if(transaction == null)
+            {
+                return Colors.Black;
+            }
+
+            if (transaction.Type == Models.TransactionType.Income)
+                return Color.FromArgb("#FF939E5A");
+            else
+                return Colors.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ AppControleFinanceiro/Converters/TransactionValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleFinanceiro.Converters
+{
+	public class TransactionValueConverter : IValueConverter
+	{
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var transaction = (Models.Transaction)value;
+
+            if (transaction == null)
+            {
+                return string.Empty;
+            }
+
+            if(transaction.Type == Models.TransactionType.Income)
+                return transaction.Value.ToString("C");
+            else
+                return $"- {transaction.Value.ToString("C")}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
