2587ba23cae128a93a0ca52d9a4bebca4b8a2df1
diegocantelli <diegocantelli19@gmail.com>
2023-02-02 09:56:42 -0300

    Adicionando converters ao projeto

---
+++ AppControleFinanceiro/Converters/TransactionNameColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleFinanceiro.Converters
+{
+    public class TransactionNameColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return Color.FromArgb("#FFFFFF");
+            }
+
+            var random = new Random();
+            var color = String.Format("#{0:X6}", random.Next(0x1000000));
+
+            return Color.FromArgb(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ AppControleFinanceiro/Converters/TransationNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleFinanceiro.Converters
+{
+    public class TransationNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var nome = (string)value;
+
+            return nome.ToUpper()[0];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
